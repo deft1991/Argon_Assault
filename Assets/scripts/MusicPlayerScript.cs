5257a223c62c96b0e52b8d3b5e516f99a085dfb3
deft <deft1991@gmail.com>
2020-11-23 21:35:59 +0300

    Add Waypoint to player camera

---
+++ Assets/scripts/MusicPlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayerScript : MonoBehaviour
+{
+    public float delay;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        _audioSource = GetComponent<AudioSource>();
+        // _audioSource.loop = true;
+        PlayMusic();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadSceneWithDelay(delay, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LoadSceneWithDelay(float delay, int sceneNumber)
+    {
+        yield return new WaitForSeconds(this.delay);
+        SceneManager.LoadScene(sceneNumber);
+    }
+    
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+ 
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
