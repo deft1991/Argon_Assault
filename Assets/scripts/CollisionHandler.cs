91fd472f5f830ce6f81643c8390e71208997c0c1
deft <deft1991@gmail.com>
2020-11-28 13:19:18 +0300

    Add collision handler

---
+++ Assets/scripts/CollisionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region PRIVATE Methods
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        SendMessage("OnPlayerDeath");
+    }
+
+    #endregion
+}
